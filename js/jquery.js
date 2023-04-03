c8e276d8b00b7e3a14b29882041327fb36d83a6a
Vladimir Kuzmin <vlasek@mail.ru>
2023-04-03 18:10:27 +0300

    d

---
+++ js/jquery.js
@@ -0,0 +1,16 @@
+
+
+let userName = promt ("Введите имя", "Имя");
+function showMessage ()
+{
+
+    let userName = promt ("Введите имя", "Имя");
+console.log("Привет, " + userName);
+}
+
+
+function release(userName) {
+    return `Привет, ${userName}!`;
+}
+
+showMessage();
